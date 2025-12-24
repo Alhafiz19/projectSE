f9446a8e270b533178a68b238f5e789674cbea9a
Alhafiz19 <alhfzalwi19@gmail.com>
2025-12-24 15:26:00 +0800

    tukar

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
-<?php
-
+// routes/web.php
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// This is where you will eventually use the database
+Route::get('/menu', function () {
+    return "This is where the food list (Database) will go later!";
 });
